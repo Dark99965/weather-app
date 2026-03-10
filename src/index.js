f30536ba7364fd9cc9fa2b09557e5b107b2e6229
Dark99965 <noah.retter@icloud.com>
2026-03-10 09:41:18 +0100

    fixed temp

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ let temp = document.getElementById('temp');
 // temp fetch
 
 fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m')
-    .then(respnse => respnse.json)
+    .then(respnse => respnse.json())
     .then(data => console.log(data.current.temperature_2m))
     
