09d8179e37a0de1b4a3216ffd7d1f23f0f6e0167
Dark99965 <noah.retter@icloud.com>
2026-03-10 19:40:28 +0100

    added wind speed var

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ let temp = document.getElementById("temp");
 
 let time = document.getElementById("time");
 
+let windspd = document.getElementById("windspeed");
+
 // temp fetch
+
 let lat = undefined;
 let longitude = undefined;
+
+// get latitude longitude
 
 navigator.geolocation.getCurrentPosition((pos) => {
   lat = pos.coords.latitude;
@@ -15,10 +20,12 @@ navigator.geolocation.getCurrentPosition((pos) => {
   setInterval(gettemp, 40000);
 });
 
+//  get temp func
+
 function gettemp() {
   /* 
       TODO: aput windo speed and humity 
-    */
+  */
   fetch(
     `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${longitude}&current=temperature_2m,weather_code,relative_humidity_2m,wind_speed_10m&forecast_days=1`,
   )
@@ -33,7 +40,11 @@ function gettemp() {
 
 // time
 
+// date var
+
 let date = new Date();
+
+// get time func
 
 function gettime() {
   const date = new Date();
