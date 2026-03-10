f8eb1a14f3f6c6901dbdfbce4c02d16ef34ddfc6
Dark99965 <noah.retter@icloud.com>
2026-03-10 17:48:04 +0100

    added lataytude and longtude

--- src/index.js
+++ src/index.js
@@ -1,33 +1,41 @@
 // var
 
-let temp = document.getElementById('temp');
+let temp = document.getElementById("temp");
 
-let time = document.getElementById('time')
+let time = document.getElementById("time");
 
 // temp fetch
+let lat = undefined;
+let longitude = undefined;
+
+navigator.geolocation.getCurrentPosition((pos) => {
+  lat = pos.coords.latitude;
+  longitude = pos.coords.longitude;
+  gettemp();
+  setInterval(gettemp, 40000);
+});
 
 function gettemp() {
-    fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m')
-        .then(re => re.json())
-        .then(data => {
-            temp.textContent = data.current.temperature_2m  + "C"
-        })
-        .catch(err => {
-            temp.textContent = err
-        })
+  fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${longitude}&current=temperature_2m`,
+  )
+    .then((re) => re.json())
+    .then((data) => {
+      temp.textContent = data.current.temperature_2m + "°C";
+    })
+    .catch((err) => {
+      temp.textContent = err;
+    });
 }
-gettemp()
-
-
-setInterval(gettemp, 40000);
 
 // time
 
-let date = new Date()
+let date = new Date();
 
 function gettime() {
-    const date = new Date()
-    time.textContent = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() 
+  const date = new Date();
+  time.textContent =
+    date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
-gettime()
-setInterval(gettime, 1000)
+gettime();
+setInterval(gettime, 1000);
